6293306dd9dd583c65d49bb19565322fef9511f1
VazheninaLN <vln.80@mail.ru>
2023-10-16 21:23:40 +0500

    модуль 9

---
+++ mantis-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+
+        protected IWebDriver driver;
+        //protected StringBuilder verificationErrors;
+        protected string baseURL;
+
+        public RegistrationHelper Registration { get; set; }
+        public FtpHelper Ftp { get; set; }
+        public MailHelper Mail { get; set; }
+
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager()
+        {
+            driver= new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            baseURL = "http://localhost";
+            Registration =  new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);    
+            Mail = new MailHelper(this);
+
+        }
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (!app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-1.3.20/login_page.php";
+                app.Value=newInstance;
+               
+            }
+            return app.Value;
+        }
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
+        }        
+
+       
+
+
+    }
+}
